1490ca97a23753ba915b76f55bc21dcd1ebf3c52
Milen <milenmihalkov@gmail.com>
2023-01-11 18:19:25 +0200

    Double Size image

--- src/js/app.js
+++ src/js/app.js
@@ -3,8 +3,8 @@ import "../scss/app.scss";
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
 
-  const button = document.querySelector(".button");
-  button.addEventListener("click", () => {
-    alert("💣");
-  });
+  var image = document.querySelector('.image');
+
+  image.addEventListener("click", () =>image.style.transform = "scale(2)");
+
 });
